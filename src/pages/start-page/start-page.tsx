cea5d855539fb704035b1ceb29b5777b05550840
Egor Yurchenko <YegorYu@yandex.ru>
2021-08-11 14:02:50 +0300

    Адаптив страницы start-page

--- src/pages/start-page/start-page.tsx
+++ src/pages/start-page/start-page.tsx
@@ -1,14 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useRef, useEffect, FC } from 'react';
+import { FC, useState, useRef, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Colors } from '../../enums/enums';
+import Context from '../../context';
 
 interface IStartPage {
     onSetColor(color: Colors): void;
 }
 
 const StartPage: FC<IStartPage> = ({ onSetColor }) => {
+    const phoneWidth = 481;
+
+    const { selectedColor } = useContext(Context);
     const [colorSelected, setColorSelected] = useState<boolean>(false);
+    const [isPhone, setIsPhone] = useState<boolean>(window.innerWidth < phoneWidth);
 
     const whiteBtn = useRef<HTMLButtonElement>(null);
     const blackBtn = useRef<HTMLButtonElement>(null);
@@ -23,12 +28,14 @@ const StartPage: FC<IStartPage> = ({ onSetColor }) => {
 
         whiteBtnElement?.addEventListener("click", setWhiteColorBtn);
         blackBtnElement?.addEventListener("click", setBlackColorBtn);
+        window.addEventListener("resize", windowResizeHandler);
 
         return () => {
             whiteBtnElement?.removeEventListener("click", setWhiteColorBtn);
             blackBtnElement?.removeEventListener("click", setBlackColorBtn);
+            window.removeEventListener("resize", windowResizeHandler);
         };
-    }, []);
+    }, [isPhone]);
 
     /**
      * Устанавливаем белый цвет фигур
@@ -36,8 +43,6 @@ const StartPage: FC<IStartPage> = ({ onSetColor }) => {
      */
     const setWhiteColorBtn = () => {
         onSetColor(Colors.White);
-        whiteBtn.current?.classList.add(classes.active);
-        blackBtn.current?.classList.remove(classes.active);
         setColorSelected(true);
     };
 
@@ -47,14 +52,40 @@ const StartPage: FC<IStartPage> = ({ onSetColor }) => {
      */
     const setBlackColorBtn = () => {
         onSetColor(Colors.Black);
-        blackBtn.current?.classList.add(classes.active);
-        whiteBtn.current?.classList.remove(classes.active);
         setColorSelected(true);
     };
 
+    /**
+     * Изменение ширины экрана - если это телефон, то приложение не работает
+     * @return {void)
+     */
+    const windowResizeHandler = () => {
+        if (window.innerWidth < phoneWidth) {
+            setIsPhone(true);
+        } else {
+            setIsPhone(false);
+        }
+    };
+
+    // Если цвет выбран, то активна соответствующая кнопка
+    const whiteBtnClasses = ["start-page__btn start-page__btn_white"];
+    const blackBtnClasses = ["start-page__btn start-page__btn_black"];
+
+    if (selectedColor === Colors.White) {
+        whiteBtnClasses.push(classes.active);
+    } else if (selectedColor === Colors.Black) {
+        blackBtnClasses.push(classes.active);
+    }
+
     // Если цвет выбран, то кнопка Submit активна
     const submitBtnClasses = ["start-page__submit"];
-    if (colorSelected) submitBtnClasses.push("active");
+    if (colorSelected) {
+        submitBtnClasses.push(classes.active);
+    }
+
+    if (isPhone) {
+        return <h1 className="start-page__phone-title">К сожалению, на телефоне приложение не работает</h1>;
+    }
 
     return (
         <div className="start-page">
@@ -63,13 +94,13 @@ const StartPage: FC<IStartPage> = ({ onSetColor }) => {
                 <button
                     ref={whiteBtn}
                     type="button"
-                    className="start-page__btn start-page__btn_white">
+                    className={whiteBtnClasses.join(" ")}>
                     Белые
                 </button>
                 <button
                     ref={blackBtn}
                     type="button"
-                    className="start-page__btn start-page__btn_black">
+                    className={blackBtnClasses.join(" ")}>
                     Чёрные
                 </button>
             </div>
